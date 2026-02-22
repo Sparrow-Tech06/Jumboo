036fb3a3ca01b81ae8b07057eadef79f01306dbe
Sparrow-Tech06 <cpal980075@gmail.com>
2026-02-22 12:18:09 +0530

    Create callback.js

---
+++ callback.js
@@ -0,0 +1,34 @@
+window.gameCallback=function(type){
+
+const overlay=document.getElementById("overlay");
+
+if(type==="rightanswer"){
+overlay.innerHTML=`
+<div class="card p-4 text-center">
+<h4 class="text-success">Correct 🎉</h4>
+<p>Great job! Keep going.</p>
+<button class="btn btn-success" onclick="next()">Next</button>
+</div>`;
+overlay.style.display="flex";
+}
+
+if(type==="wronganswer"){
+overlay.innerHTML=`
+<div class="card p-4 text-center">
+<h4 class="text-danger">Try again</h4>
+<p>You are very close 🔥</p>
+<button class="btn btn-primary" onclick="closePop()">Continue</button>
+</div>`;
+overlay.style.display="flex";
+}
+
+}
+
+function next(){
+closePop();
+goNext();
+}
+
+function closePop(){
+overlay.style.display="none";
+}
